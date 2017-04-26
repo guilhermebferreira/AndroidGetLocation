4b4cbd8f1dd31f97ae6464e23496abcaa5059183
Guilherme <guilherme@uft.edu.br>
2017-04-26 02:29:08 -0300

    add map click listener and action

--- app/src/main/java/io/github/guilhermebferreira/getlocation/MapLocation.java
+++ app/src/main/java/io/github/guilhermebferreira/getlocation/MapLocation.java
@@ -39,7 +39,6 @@ public class MapLocation extends FragmentActivity implements OnMapReadyCallback,
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_location);
 
-        checkFineLocationPermission();
 
         txtLatitude = (EditText) findViewById(R.id.txtLatitude);
         txtLongitude = (EditText) findViewById(R.id.txtLongitude);
@@ -48,6 +47,7 @@ public class MapLocation extends FragmentActivity implements OnMapReadyCallback,
 
         addListenerOnButton();
 
+        //getting map fragment
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -55,49 +55,18 @@ public class MapLocation extends FragmentActivity implements OnMapReadyCallback,
         locationService = new FusedLocationListener(this, this);
     }
 
-    private void checkFineLocationPermission(){
-        if (ContextCompat.checkSelfPermission(this,
-                ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    ACCESS_FINE_LOCATION)) {
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{ACCESS_FINE_LOCATION},
-                        MY_PERMISSION_REQUEST_READ_FINE_LOCATION);
-
-                // MY_PERMISSION_REQUEST_READ_FINE_LOCATION is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        }
-    }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         Log.d("I", "Map ready");
+
+
+        addListenerOnMap();
         // Add a marker in Sydney and move the camera
         /*LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
@@ -134,6 +103,27 @@ public class MapLocation extends FragmentActivity implements OnMapReadyCallback,
 
     }
 
+    public void addListenerOnMap(){
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener()
+        {
+            @Override
+            public void onMapClick(LatLng position)
+            {
+
+                mMap.addMarker(new MarkerOptions().position(position).title("Click location"));
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position,8));
+
+
+                txtLatitude.setText(String.valueOf(position.latitude));
+                txtLongitude.setText(String.valueOf(position.longitude));
+
+
+                Log.d("I", "Click position defined");
+            }
+        });
+    }
+
     public void addListenerOnButton() {
 
 
@@ -158,28 +148,5 @@ public class MapLocation extends FragmentActivity implements OnMapReadyCallback,
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSION_REQUEST_READ_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
 }
